c131916f465aea64512823719d61ed44e5bef256
QobiljonIbaydullayevDev <qobiljonyoqubovich2002@gmail.com>
2023-11-08 10:09:46 +0500

    .

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
 
-document.querySelector('.slider-next').addEventListener('click', function(){
+const next = document.querySelector('.slider-next').addEventListener('click', function(){
     offset = offset +512;
     if(offset > 1536){
         offset=0;
     }
     sliderLine.style.left = -offset + 'px'
+    console.log(next);
+    console.log(sliderLine);
     console.log(offset);
 })
 
-document.querySelector('.slider-prew').addEventListener('click', function(){
+const prev = document.querySelector('.slider-prew').addEventListener('click', function(){
     offset = offset -512;
     if(offset < 0){
         offset=1536;
     }
     sliderLine.style.left = -offset + 'px'
 
+    console.log(next);
     console.log(sliderLine);
+    console.log(offset);
 })
